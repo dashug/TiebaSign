e800288960afce5cc53112addaf5fd88bbe40aa2
dashug <74298454+dashug@users.noreply.github.com>
2024-06-03 16:35:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -174,10 +174,10 @@ def client_sign(bduss, tbs, fid, kw):
     return res
 
 def send_email(sign_list):
-    if ('HOST' not in ENV or 'FROM' not in ENV or 'TO' not in ENV or 'AUTH' not in ENV):
+    if 'HOST' not in ENV or 'FROM' not in ENV or 'TO' not in ENV or 'AUTH' not in ENV:
         logger.error("未配置邮箱")
         return
-    HOST = ENV['smtp.126.com']
+    HOST = ENV['HOST']
     FROM = ENV['FROM']
     TO = ENV['TO'].split('#')
     AUTH = ENV['AUTH']
